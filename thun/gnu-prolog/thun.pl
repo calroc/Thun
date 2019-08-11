c8c1df8d882009c3a466ec16f1e3cfd525c37d12
Simon Forman <sforman@hushmail.com>
2019-08-10 20:12:41 -0700

    Bring in funcs and combos.

--- thun/gnu-prolog/thun.pl
+++ thun/gnu-prolog/thun.pl
@@ -17,8 +17,9 @@
     along with Thun.  If not see <http://www.gnu.org/licenses/>.
 
 */
-:- dynamic(func/3).
-:- discontiguous(func/3).
+% :- dynamic(func/3).
+% :- discontiguous(func/3).
+
 
 :- initialization(loop).
 
@@ -84,6 +85,35 @@ func(cons, [A, B|S], [[B|A]|S]).
 func(swap, [A, B|S],  [B, A|S]).
 func(dup,     [A|S],  [A, A|S]).
 func(pop,     [_|S],        S ).
+func(sqrt, [A|S], [sqrt(A)|S]).
+
+func(concat, [A, B|S],   [C|S]) :- append(B, A, C).
+func(flatten,   [A|S],   [B|S]) :- flatten(A, B).
+func(swaack,    [R|S],   [S|R]).
+func(stack,        S ,   [S|S]).
+func(clear,        _ ,      []).
+func(first, [[X|_]|S],   [X|S]).
+func(rest,  [[_|X]|S],   [X|S]).
+func(unit,      [X|S], [[X]|S]).
+
+func(rolldown, [A, B, C|S], [B, C, A|S]).
+func(dupd,        [A, B|S], [A, B, B|S]).
+func(over,        [A, B|S], [B, A, B|S]).
+func(tuck,        [A, B|S], [A, B, A|S]).
+
+func(shift, [[B|A], C|D], [A, [B|C]|D]).
+
+func(rollup, Si, So) :- func(rolldown, So, Si).
+func(uncons, Si, So) :- func(cons, So, Si).
+
+func(bool, [    0|S], [false|S]) :- !.
+func(bool, [  0.0|S], [false|S]) :- !.
+func(bool, [   []|S], [false|S]) :- !.
+func(bool, [   ""|S], [false|S]) :- !.
+func(bool, [false|S], [false|S]) :- !.
+
+func(bool, [_|S], [true|S]).
+
 
 
 /*
@@ -95,6 +125,48 @@ combo(dip,     [P, X|S], S, Ei, Eo) :- append(P, [X|Ei], Eo).
 combo(dipd, [P, X, Y|S], S, Ei, Eo) :- append(P, [Y, X|Ei], Eo).
 
 combo(dupdip, [P, X|S], [X|S], Ei, Eo) :- append(P, [X|Ei], Eo).
+combo(dupdip, [P, X|S], [X|S], Ei, Eo) :- append(P, [X|Ei], Eo).
+
+combo(branch, [T, _,  true|S], S, Ei, Eo) :- append(T, Ei, Eo).
+combo(branch, [_, F, false|S], S, Ei, Eo) :- append(F, Ei, Eo).
+combo(branch, [T, F,  Expr|S], S, Ei, Eo) :-
+    \+ Expr = true, \+ Expr = false,
+    catch(  % Try Expr and do one or the other,
+        (Expr -> append(T, Ei, Eo) ; append(F, Ei, Eo)),
+        _,  % If Expr don't grok, try both branches.
+        (append(T, Ei, Eo) ; append(F, Ei, Eo))
+        ).
+
+combo(loop, [_, false|S], S, E,  E ).
+combo(loop, [B,  true|S], S, Ei, Eo) :- append(B, [B, loop|Ei], Eo).
+combo(loop, [B,  Expr|S], S, Ei, Eo) :-
+    \+ Expr = true, \+ Expr = false,
+    catch(  % Try Expr and do one or the other,
+        (Expr -> append(B, [B, loop|Ei], Eo) ; Ei=Eo),
+        _,  % If Expr don't grok, try both branches.
+        (Ei=Eo ; append(B, [B, loop|Ei], Eo))
+        ).
+
+combo(step, [_,    []|S],    S,  E,  E ).
+combo(step, [P,   [X]|S], [X|S], Ei, Eo) :- !, append(P, Ei, Eo).
+combo(step, [P, [X|Z]|S], [X|S], Ei, Eo) :-    append(P, [Z, P, step|Ei], Eo).
+
+combo(times, [_, 0|S], S, E,  E ).
+combo(times, [P, 1|S], S, Ei, Eo) :- append(P, Ei, Eo).
+combo(times, [P, N|S], S, Ei, Eo) :- N #>= 2, M #= N - 1, append(P, [M, P, times|Ei], Eo).
+combo(times, [_, N|S], S, _,  _ ) :- N #< 0, fail.
+
+combo(genrec, [R1, R0, Then, If|S],
+              [  Else, Then, If|S], E, [ifte|E]) :-
+    Quoted = [If, Then, R0, R1, genrec],
+    append(R0, [Quoted|R1], Else).
+
+
+ 
+
+
+
+
 
 
 
