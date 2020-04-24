40a78306943d74aaea457c63c5eaa788e1418c5b
Simon Forman <sforman@hushmail.com>
2020-04-24 11:45:16 -0700

    Fixes #40353 TypeError: must be str or None, not bytes

--- joy/gui/world.py
+++ joy/gui/world.py
@@ -142,7 +142,7 @@ class StackDisplayWorld(World):
     self.repo.stage([self.relative_STACK_FN])
     commit_id = self.repo.do_commit(
       'message',
-      committer='Simon Forman <forman.simon@gmail.com>',
+      committer=b'Simon Forman <forman.simon@gmail.com>',
       )
     _log.info('commit %s', commit_id)
 
