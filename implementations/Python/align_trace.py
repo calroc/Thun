fab172daf4eedd038e4ecfec22bf8bbaf215f455
Simon Forman <sforman@hushmail.com>
2022-09-19 13:25:09 -0700

    Minor cleanup.

--- implementations/Python/align_trace.py
+++ implementations/Python/align_trace.py
@@ -3,12 +3,11 @@ import sys
 SEP = '•'
 
 lines = sys.stdin.readlines()
-
-indicies = [line.index(SEP) for line in lines if SEP in line]
+indicies = [line.find(SEP) for line in lines]
 
 MAX = max(indicies)
 
-prefix_counts = [MAX - i for i in indicies]
+prefix_counts = (MAX - i for i in indicies)
 
 for count, line in zip(prefix_counts, lines):
     print(' ' * count, line, sep='', end = '')
