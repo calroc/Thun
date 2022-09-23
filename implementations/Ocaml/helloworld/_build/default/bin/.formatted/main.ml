64d4a429789fd27b6363de81b17c7048628bdf27
Simon Forman <sforman@hushmail.com>
2022-09-23 08:38:17 -0700

    autoformat

--- implementations/Ocaml/helloworld/_build/default/bin/.formatted/main.ml
+++ implementations/Ocaml/helloworld/_build/default/bin/.formatted/main.ml
@@ -5,13 +5,20 @@ type joy_type =
   | JoyInt of int
   | JoyList of joy_type list
 
-type joy_list = joy_type list
+(* type joy_list = joy_type list *)
 
 let joy_true = JoyTrue
 let joy_false = JoyFalse
 let j_loop = JoySymbol "loop"
-let empty_list = JoyList []
 let zero = JoyInt 0
-let dummy = [ joy_true; joy_false; j_loop; zero ]
-let list_get jt = match jt with JoyList el -> el | _ -> []
-let () = print_endline "Hello, World!"
+let dummy = JoyList [ joy_true; joy_false; j_loop; zero ]
+
+let rec joy_to_string jt =
+  match jt with
+  | JoySymbol sym -> sym
+  | JoyTrue -> "true"
+  | JoyFalse -> "false"
+  | JoyInt i -> string_of_int i
+  | JoyList el -> "[" ^ String.concat " " (List.map joy_to_string el) ^ "]"
+
+let () = print_endline (joy_to_string dummy)
--- implementations/Ocaml/helloworld/bin/main.ml
+++ implementations/Ocaml/helloworld/bin/main.ml
@@ -14,11 +14,11 @@ let zero = JoyInt 0
 let dummy = JoyList [ joy_true; joy_false; j_loop; zero ]
 
 let rec joy_to_string jt =
-    match jt with
-    | JoySymbol sym -> sym
-    | JoyTrue -> "true"
-    | JoyFalse -> "false"
-    | JoyInt i -> string_of_int i
-    | JoyList el -> "[" ^ (String.concat " " (List.map joy_to_string el)) ^ "]";;
+  match jt with
+  | JoySymbol sym -> sym
+  | JoyTrue -> "true"
+  | JoyFalse -> "false"
+  | JoyInt i -> string_of_int i
+  | JoyList el -> "[" ^ String.concat " " (List.map joy_to_string el) ^ "]"
 
 let () = print_endline (joy_to_string dummy)
