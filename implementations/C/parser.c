2f71db945aff1cd8a1e843b1a87d404133f45c09
Simon Forman <sforman@hushmail.com>
2023-02-01 16:40:14 -0800

    trim_leading_blanks

--- implementations/C/parser.c
+++ implementations/C/parser.c
@@ -2,17 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
-const char *text = "hi there friend";
+const char *BLANKS = " \t";
+const char *TEXT = " hi there friend";
 /*                  01234567890123456789
-                      ^     ^
+                       ^     ^
 */
+
+char *
+trim_leading_blanks(char *str)
+{
+        size_t offset = strspn(str, BLANKS);
+        if (offset == strlen(str)) {
+                /* All blanks. */
+                return NULL;
+        }
+        return str + offset;
+}
 
 int
 main(void)
 {
-	char *rest;
+	char *rest, *text;
 	ptrdiff_t diff;
 
+        text = trim_leading_blanks((char *)TEXT);
 	rest = strpbrk(text, " []");
 	while (NULL != rest) {
 		diff = rest - text;
