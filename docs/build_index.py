70aa2ef5e952b9a107d4be33f031f3767b327a60
sforman <sforman@hushmail.com>
2023-08-13 14:30:57 -0700

    Working on the website for Thun.
    I'm going to eventually update the old Sphinx-generated site on OSDN.

--- docs/build_index.py
+++ docs/build_index.py
@@ -1,10 +1,15 @@
 import sys
 import markdown
 
-#filename = './README.md'
+#filename = './source/Thun.md'
 filename = sys.argv[-1]
-with open(filename) as f:
+with open(filename, encoding="UTF_8") as f:
     text = f.read()
+
+def title_of(html):
+    return html[html.find('<h1>') + 4:html.find('</h1>')]
+
+#title = title_of(text)
 
 html = markdown.markdown(text, output_format="html5")
 
@@ -13,7 +18,7 @@ print(f'''\
 <html>
 <head>
 <meta charset="utf-8">
-<title>Thun</title>
+<title>{title_of(html)}</title>
 <link rel="stylesheet" href="/css/fonts.css">
 <link rel="stylesheet" href="/css/site.css">
 </head>
